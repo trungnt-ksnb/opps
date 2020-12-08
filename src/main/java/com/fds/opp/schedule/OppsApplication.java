e4c733769512c9faca679f89680adceb5a840116
trungnt <trungnt.fds.vn>
2020-12-08 14:12:29 +0700

    init project

---
+++ src/main/java/com/fds/opp/schedule/OppsApplication.java
@@ -0,0 +1,13 @@
+package com.fds.opp.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fds/opp/schedule/OppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fds.opp.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
