05403a72ef422a632b0fa017b3a78f89c31f7949
Dinh Doan Viet <doanvietcntt99@gmail.com>
2020-12-09 17:34:47 +0700

    CRUD Project

--- src/main/java/com/fds/opp/app/controller/ProjectController.java
+++ src/main/java/com/fds/opp/app/controller/ProjectController.java
@@ -16,21 +16,72 @@ public class ProjectController {
     public void crud() throws Exception {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
-
-        create(session);
-
+//        create(session);
+//        Project pj = new Project();
+//        pj.setIdProject(1);
+//        pj.setNameProject("Updating Project");
+//        pj.setDescriptionProject("Updated Complete!");
+//        update(session,pj);
+//        Project pj_Delete = new Project();
+//        pj_Delete.setNameProject("Project Test 1");
+//        pj_Delete.setIdProject(34);
+//        pj_Delete.setDescriptionProject("");
+//        delete(session, pj_Delete);
+        read(session, 1);
         session.close();
     }
     public void create(Session session) throws Exception
     {
-        System.out.println("Creating Project...");
+        System.out.println("Creating Table Project...");
         List<Project> listProject = ProjectSync.getListProjectFromAPI();
         for (Project project: listProject) {
             session.beginTransaction();
             session.save(project);
             session.getTransaction().commit();
         }
-
+    }
+    public void update(Session session, Project project) throws Exception
+    {
+        try{
+            System.out.println("Updating Table Project...");
+            Project projectAfterUpdate = (Project) session.get(Project.class, project.getIdProject());
+            projectAfterUpdate.setNameProject(project.getNameProject());
+            projectAfterUpdate.setDescriptionProject(project.getDescriptionProject());
+            session.beginTransaction();
+            session.saveOrUpdate(projectAfterUpdate);
+            session.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void delete(Session session, Project project) throws Exception
+    {
+        try{
+            System.out.println("Deleting Table Project...");
+            Project projectAfterDelete = (Project) session.get(Project.class, project.getIdProject());
+            session.beginTransaction();
+            session.delete(projectAfterDelete);
+            session.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void read(Session session, Integer id)
+    {
+        try
+        {
+            System.out.println("Reading Database");
+            Project findProject = (Project) session.get(Project.class, id);
+            System.out.println(findProject);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
