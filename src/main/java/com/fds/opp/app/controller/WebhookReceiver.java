c226d83b11565e1c53e8f3f14f8b057216265aee
minhsama <minhsama2000@gmail.com>
2020-12-09 16:47:51 +0700

    update by Minh

--- src/main/java/com/fds/opp/app/controller/WebhookReceiver.java
+++ src/main/java/com/fds/opp/app/controller/WebhookReceiver.java
@@ -3,6 +3,6 @@ package com.fds.opp.app.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class WebhookReceiver {
+	
 }
--- src/main/java/com/fds/opp/app/model/Account.java
+++ src/main/java/com/fds/opp/app/model/Account.java
@@ -38,7 +38,68 @@ public class Account extends AccountAudit {
     @Column(name = "botId")
     private Integer botId;
     @Column(name = "status")
-    private Boolean status;
+    private String status;
+	public Integer getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getCustomField() {
+		return customField;
+	}
+	public void setCustomField(String customField) {
+		this.customField = customField;
+	}
+	public Integer getBotId() {
+		return botId;
+	}
+	public void setBotId(Integer botId) {
+		this.botId = botId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-
+    
+    
 }
